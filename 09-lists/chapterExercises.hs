871d8818f4277397f7a4505766502a28a791c678
Daniel J. Rollins <daniel@djrollins.com>
2018-01-21 18:32:55 +0000

    chapter 9: lists

---
+++ 09-lists/chapterExercises.hs
@@ -0,0 +1,17 @@
+module ChapterExercises where
+
+import Data.Char
+
+allUppers :: String -> String
+allUppers = filter isUpper
+
+capitalise :: String -> String
+capitalise "" = ""
+capitalise (c:cs) = toUpper c : cs
+
+shout :: String -> String
+shout "" = ""
+shout (c:cs) = toUpper c : shout cs
+
+firstCapitalisedLetter :: String -> Char
+firstCapitalisedLetter = toUpper . head
